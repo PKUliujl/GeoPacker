ced1d19359a1acb0088b5e355fa36a599eed5ae1
PKUliujl <110894511+PKUliujl@users.noreply.github.com>
2022-08-10 16:03:59 +0800

    Update run_argparse.py

--- common/run_argparse.py
+++ common/run_argparse.py
@@ -12,7 +12,7 @@ def run_inputparameters():
     parser.add_argument('--chainID',type=str,required=True, help='a protein chain to be packered, eg. A')
     parser.add_argument('--seqfile',type=str,help='a fasta format file including the sequences to be designed')
     parser.add_argument('--outputPATH','-oP',type=str,help='the directory path of the outputfile.  default: inputPATH')
-    parser.add_argument('--outputfile','-o',type=str,help='the name of output file.  default: pdbname_chainID_repackered.pdb')
+    parser.add_argument('--outputfile','-o',type=str,help='the name of output file.  default: pdbname_chainID_repacked.pdb')
     args = parser.parse_args()
     
     if args.purpose == 1:
